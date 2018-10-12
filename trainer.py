9332fb51c13374f11f861273dae8184e59a09f2f
merak <2692195809@qq.com>
2018-10-12 17:41:54 +0800

    矩阵化，重新标记

--- trainer.py
+++ trainer.py
@@ -5,6 +5,7 @@
 # @File     : trainer.py
 # @Software : PyCharm
 import entropy
+import numpy as np
 
 
 def train(data: dict, data_size: int) -> None:
@@ -29,6 +30,37 @@ def compute_entropy(data: dict, m: int) -> float:
     return ent
 
 
+def compute_node(info: dict):
+    """
+    :param info: 一阶dict，key为0/1
+    :return:
+    """
+    data_set = []
+    for i in info.values():
+        data_set += i
+    feature_num = len(data_set[0]) - 1  # 最后一项是最原始的分类
+    data_set = np.array(data_set)[:, :-1]  # 变为矩阵
+    print(data_set)
+    signs = []
+    for sign in info.keys():
+        signs += [sign] * len(info[sign])
+    print(signs)
+    data_set = np.c_[data_set, signs]  # 附加标记，在最后一列
+    # print(data_set)
+    # print(feature_num)
+    for feature in range(feature_num):
+
+        break
+
+
+
+
+
 if __name__ == '__main__':
     # train(1)
+    a = {
+        'a': [[0, 1], [1, 2]],
+        'b': [[5, 6], [9, 10]]
+    }
+    compute_node(a)
     pass
