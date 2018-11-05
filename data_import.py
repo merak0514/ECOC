4b537f1e249f814124d7e8c7f4f8508d51fdaa38
merak <2692195809@qq.com>
2018-11-05 11:26:21 +0800

    从一开始划分验证集

--- data_import.py
+++ data_import.py
@@ -7,6 +7,7 @@
 
 import csv
 from collections import defaultdict
+import operation as op
 
 
 def get_data(show: bool = False) -> list:
@@ -29,11 +30,15 @@ def get_data(show: bool = False) -> list:
             continue
         datum = list(map(float, i))
         origin_data.append(datum)
+    train_data, validation_data = op.hold_validation_data(origin_data)
+    for datum in train_data:
         classes[int(datum[-1])].append(datum)
     class_names = list(classes.keys())
     if show is True:
-        print(feature_name_set)
-        print(class_names)
-        print(classes)
-        print(origin_data)
-    return [feature_name_set, class_names, classes, origin_data]
+        print('feature_name_set', feature_name_set)
+        print('class_names', class_names)
+        print('type of classes', type(classes))
+        print('origin_data', origin_data[0])
+        print('train_data', train_data[0])
+        print('validation_data', validation_data[0])
+    return [feature_name_set, class_names, classes, origin_data, train_data, validation_data]
--- operation.py
+++ operation.py
@@ -80,3 +80,31 @@ def hold_out(data: list, show=False) -> tuple:
         print('test', len(test_data))
         print('validation', len(validation_data))
     return train_data, test_data, validation_data
+
+
+def hold_validation_data(data: list, show=False) -> tuple:
+    """
+    将原始数据集分为train, validation
+    :param data:
+    :param show:
+    :return: （train_data, test_data, validation_data）
+    """
+    train_data = []
+    validation_data = []
+    rate = [0.7, 0.3]
+    data = np.array(data)
+    label = list(data[:, -1])
+    label_dict = {i: label.count(i) for i in set(label)}
+    used_label = {i: 0 for i in set(label)}
+    np.random.shuffle(data)
+    for i in data:
+        sign = i[-1]
+        if used_label[sign] < rate[0] * label_dict[sign]:
+            train_data.append(i)
+            used_label[sign] += 1
+        else:
+            validation_data.append(i)
+    if show is True:
+        print('train', len(train_data))
+        print('validation', len(validation_data))
+    return train_data, validation_data
